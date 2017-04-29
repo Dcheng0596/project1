d3e155afca30168d57f8e79b963ab434aecad85b
Darien <Darien@192.168.1.10>
2017-04-29 18:21:47 -0400

    Added negative button and Slider working

--- Base4Calculator/src/Base4Panel.java
+++ Base4Calculator/src/Base4Panel.java
@@ -2,6 +2,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
@@ -9,15 +11,18 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Base4Panel extends JPanel {
     private Base4CalcState calc; // this object will actually do the calculating work
 	JButton zero, one, two, three, four, five, six, seven, eight, nine, A, B, C, D, E, F,  
-	plus, minus, multiply, divide, clear, equal;
+	plus, minus, multiply, divide, clear, equal, negative;
     JTextField screen;
     JSlider base;
 	
 	Base4Panel() {
+	    
 	    calc = new Base4CalcState();
 		this.setLayout(new FlowLayout()); 
 		zero = new JButton("0"); 
@@ -42,8 +47,13 @@ public class Base4Panel extends JPanel {
 		divide = new JButton("/"); 
 		clear = new JButton("Clear");
 		equal = new JButton("=");
+		negative = new JButton("(-)");
 		screen = new JTextField(24);
-		base = new JSlider(2, 16, 10);
+		base = new JSlider(2, 16, 10); // set slider to 10 by default
+		A.setEnabled(false); B.setEnabled(false); 
+		C.setEnabled(false); D.setEnabled(false);
+		E.setEnabled(false); F.setEnabled(false);
+		
 		
 		A.addActionListener(new NumberListener());
 		B.addActionListener(new NumberListener());
@@ -61,14 +71,15 @@ public class Base4Panel extends JPanel {
 		two.addActionListener(new NumberListener());
 		one.addActionListener(new NumberListener());
 		zero.addActionListener(new NumberListener());
+		negative.addActionListener(new NegativeListener());
 		plus.addActionListener(new OperationListener());
 		minus.addActionListener(new OperationListener());
 		multiply.addActionListener(new OperationListener());
 		divide.addActionListener(new OperationListener());
 		clear.addActionListener(new ClearListener());
 		equal.addActionListener(new EqualListener());
+		base.addChangeListener(new SliderListener());
 		
-			
 		add(screen);
 		add(base);
 		add(plus);
@@ -92,12 +103,17 @@ public class Base4Panel extends JPanel {
 		add(one);
 		add(zero);
 		add(clear); 
+		add(negative);
 		add(equal);
 		
 		screen.setEditable(false); 
 		screen.setHorizontalAlignment(SwingConstants.RIGHT); // right justify screen
 		screen.setText("0");    // displays 0 by default
-		base.setPreferredSize(new Dimension(250, 20)); // set slider size
+		base.setPreferredSize(new Dimension(250, 40)); // set slider size
+		base.setPaintTicks(true);
+		base.setMajorTickSpacing(1);
+		base.setPaintLabels(true);
+		base.setLabelTable(base.createStandardLabels(2));
 
 	}
     class NumberListener implements ActionListener
@@ -152,21 +168,88 @@ public class Base4Panel extends JPanel {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            int value1 = Integer.parseInt(calc.getTotalValue(), 10);        // convert total and temp value to base 10
-            int value2 = Integer.parseInt(calc.getTempValue(), 10);  
+            try
+            {
+                long value1 = Long.parseLong(calc.getTotalValue(), calc.getBase());        // convert total and temp value to base 10
+                long value2 = Long.parseLong(calc.getTempValue(), calc.getBase()); 
+            }
+            catch(NumberFormatException ex)
+            {
+                return;   // Does nothing if total or temp is empty string
+            }
+            long value1 = Long.parseLong(calc.getTotalValue(), calc.getBase());        // convert total and temp value to base 10
+            long value2 = Long.parseLong(calc.getTempValue(), calc.getBase()); 
             String sum = new String();      // contains new total
             if(calc.getOperation().equals("+"))     // sets sum equal to the combined values converted to the desired base as a string
-                sum = new String(Integer.toString((value1 + value2), calc.getBase()));
+                sum = new String(Long.toString((value1 + value2), calc.getBase()));
             if(calc.getOperation().equals("-"))
-                sum = new String(Integer.toString((value1 - value2), calc.getBase()));
+                sum = new String(Long.toString((value1 - value2), calc.getBase()));
             if(calc.getOperation().equals("x"))
-                sum = new String(Integer.toString((value1 * value2), calc.getBase()));
+                sum = new String(Long.toString((value1 * value2), calc.getBase()));
             if(calc.getOperation().equals("/"))
-                sum = new String(Integer.toString((value1 / value2), calc.getBase()));
+                sum = new String(Long.toString((value1 / value2), calc.getBase()));
             calc.clearTotalValue();
             calc.setTotalValue(sum);             // update current total  
             calc.clearTempValue();
-            screen.setText(calc.getTotalValue());   // shows the current total
+            screen.setText(calc.getTotalValue().toUpperCase());   // shows the current total and formats string to uppercase
+        }
+    }
+    class SliderListener implements ChangeListener
+    {
+        JButton[] digitList = new JButton[]{two, three, four, five, six, seven, eight, nine, A, B, C, D, E, F}; // array of varaiblew digits
+        @Override
+        public void stateChanged(ChangeEvent e)
+        {
+            
+            int baseNum = base.getValue();
+            int preBase = calc.getBase();
+            calc.setBase(baseNum); 
+            if(!screen.getText().equals("0"))
+            {
+                Long decimal = Long.parseLong(screen.getText(), preBase);
+                String string = new String(Long.toString(decimal, baseNum).toUpperCase());
+                screen.setText(string);
+          
+                if(calc.getOperation().equals(""))
+                { 
+                    calc.clearTotalValue();
+                    calc.setTotalValue(string);  
+                }
+                else
+                {
+                    calc.clearTempValue();
+                    calc.setTempValue(string);  
+                } 
+            }
+            for(int i = 0; i < 14; i++)     // enables or disables buttons depending on slider value
+            {
+                if(i < baseNum - 2)
+                    digitList[i].setEnabled(true);
+                else
+                    digitList[i].setEnabled(false);      
+            }  
+        }
+    }
+    
+    class NegativeListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            if(calc.getOperation().isEmpty())
+            {
+                String temp = new String("-" + calc.getTotalValue());   // turns string negative and displays it on screen
+                calc.clearTotalValue();
+                calc.setTotalValue(temp);
+                screen.setText(calc.getTotalValue());
+            }
+            else
+            {
+                String temp = new String("-" + calc.getTempValue());
+                calc.clearTempValue();
+                calc.setTempValue(temp);
+                screen.setText(calc.getTempValue());
+            }
         }
     }
 	// you need to deal with event handling. before you go too crazy writing code, 
